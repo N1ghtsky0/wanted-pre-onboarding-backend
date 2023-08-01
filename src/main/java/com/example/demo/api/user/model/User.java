a15f220d0c8313258072e192eb7701458aad6738
Ji-Wook, Kim <90381800+N1ghtsky0@users.noreply.github.com>
2023-08-01 21:47:53 +0900

    [create] 생성, 수정 시간 컬럼

--- src/main/java/com/example/demo/api/user/model/User.java
+++ src/main/java/com/example/demo/api/user/model/User.java
@@ -5,12 +5,18 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Getter
+@EntityListeners(AuditingEntityListener.class)
 public class User {
 
     @Id
@@ -28,5 +34,12 @@ public class User {
 
     @Column(nullable = true)
     private String nickName;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 
 }
