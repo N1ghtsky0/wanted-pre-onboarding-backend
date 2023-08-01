56cf1578d6f15afa7241d44e03653c94b2f87898
Ji-Wook, Kim <90381800+N1ghtsky0@users.noreply.github.com>
2023-08-01 21:37:09 +0900

    [delete] 사용하지 않는 import 문

--- src/main/java/com/example/demo/api/auth/controller/AuthController.java
+++ src/main/java/com/example/demo/api/auth/controller/AuthController.java
@@ -2,7 +2,6 @@ package com.example.demo.api.auth.controller;
 
 import com.example.demo.api.auth.dto.RequestRegister;
 import com.example.demo.api.auth.service.AuthService;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
